a544b2301e9ce066b09b40ab8fb5d9171ec4c446
apilbasnet <117279433+apilbasnet@users.noreply.github.com>
2023-06-28 07:45:41 +0545

    spinner

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { FaSearch } from "react-icons/fa"
-import { Table, Badge, Dropdown } from 'react-bootstrap'
+import { Table, Badge, Dropdown, Spinner } from 'react-bootstrap'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function SearchBar() {
+    const [loaded, setLoaded] = useState(false)
     const [product, setProduct] = useState([])
     const [isFocused, setIsFocused] = useState(false);
     const [filteredData, setFilteredData] = useState([]);
@@ -24,7 +25,9 @@ function SearchBar() {
         axios.get('https://fakestoreapi.com/products')
             .then((response) => {
                 setProduct(response.data);
-                setFilteredData(response.data)
+                setFilteredData(response.data);
+                setLoaded(true);
+                console.log(loaded);
 
             })
             .catch(err => console.log("Couldn't fetch the data." + err));
@@ -53,7 +56,7 @@ function SearchBar() {
 
 
 
-    }, [])
+    }, [loaded])
 
 
 
@@ -68,7 +71,7 @@ function SearchBar() {
     const handleCategoryChange = (category) => {
 
         setSelectedCategory(category);
-        filterData(category,selectedPrice);
+        filterData(category, selectedPrice);
         // setSelectedPrice("None")
     }
     const handlePriceChange = (price) => {
@@ -87,21 +90,21 @@ function SearchBar() {
         } else {
             setFilteredData(
                 product.filter(
-                    (x) =>{
+                    (x) => {
                         // x.title.toLowerCase().includes(selectedCategory.toLowerCase()) &&
-                        const categoryMatch = category ==="None" || x.category.toLowerCase() === category.toLowerCase();
-                        const priceMatch = 
-                        (price === "None") ||
-                        (price === 20 && x.price < 20) ||
-                        (price === 50 && x.price < 50) ||
-                        (price === 100 && x.price < 100) ||
-                        (price === 200 && x.price < 200) ||
-                        (price === 500 && x.price < 500) ||
-                        (price === 700 && x.price < 700) ||
-                        (price === 1000 && x.price < 1000) ||
-                        (price === 5000 && x.price < 5000);
-                        return categoryMatch && priceMatch ; 
-                        
+                        const categoryMatch = category === "None" || x.category.toLowerCase() === category.toLowerCase();
+                        const priceMatch =
+                            (price === "None") ||
+                            (price === 20 && x.price < 20) ||
+                            (price === 50 && x.price < 50) ||
+                            (price === 100 && x.price < 100) ||
+                            (price === 200 && x.price < 200) ||
+                            (price === 500 && x.price < 500) ||
+                            (price === 700 && x.price < 700) ||
+                            (price === 1000 && x.price < 1000) ||
+                            (price === 5000 && x.price < 5000);
+                        return categoryMatch && priceMatch;
+
                     }
                 )
             );
@@ -116,7 +119,7 @@ function SearchBar() {
             (selectedCategory === 'None' || x.category.toLowerCase() === selectedCategory.toLowerCase()) &&
             (selectedPrice === 'None' || x.price <= selectedPrice)
         ));
-        
+
     }
 
 
@@ -126,127 +129,151 @@ function SearchBar() {
 
     return (
         <>
-            <div className='   w-[80%] h-[80%] m-auto   flex flex-col justify-center items-center'>
+
+            {!loaded ?
 
 
-                <div className={` my-3 relative w-[40rem]  `}>
+                <div className='h-screen flex justify-center items-center'>
 
-                    <div className="absolute top-3 left-3 ">
-                        <FaSearch />
-                    </div>
+                    <Spinner className='' animation="border" role="status">
+                        <span className='cisually-hidden'>
 
-                    <input
-                        id="searchInput"
-                        type="text"
-                        placeholder="Search"
-                        className={` pl-10 pr-4 py-2 w-full  border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-x-zinc-900`}
-                        onFocus={handleFocus}
-                        onBlur={handleBlur}
-                        onChange={handleSearch}
-                    />
+                        </span>
+
+                    </Spinner>
+
+                </div>
 
 
-                    { !isFocused && (
+                :
+                <div className='   w-[80%] h-[80%] m-auto   flex flex-col justify-center items-center'>
 
-                        <div className='absolute top-2 right-2 border border-gray-900  rounded-lg px-1  ' aria-disabled >
-                            <p className='text-neutral-800'>Ctrl K</p>
+
+                    <div className={` my-3 relative w-[40rem]  `}>
+
+                        <div className="absolute top-3 left-3 ">
+                            <FaSearch />
+                        </div>
+
+                        <input
+                            id="searchInput"
+                            type="text"
+                            placeholder="Search"
+                            className={` pl-10 pr-4 py-2 w-full  border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-x-zinc-900`}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                            onChange={handleSearch}
+                        />
+
+
+                        {!isFocused && (
+
+                            <div className='absolute top-2 right-2 border border-gray-900  rounded-lg px-1  ' aria-disabled >
+                                <p className='text-neutral-800'>Ctrl K</p>
+                            </div>
+
+
+
+                        )}
+
+
+
+                        <div className='flex'>
+                            <label className='mt-2 mr-2 text-sm  text-neutral-500 '> Category
+
+                                <Dropdown >
+                                    <Dropdown.Toggle className='bg-zinc-500 min-w-[11rem] flex justify-between items-center' variant="secondary" id="dropdown-basic">
+                                        {selectedCategory}
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item onClick={() => handleCategoryChange('None')}>None</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handleCategoryChange("Men's clothing")} >Men's Clothing</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handleCategoryChange("Jewelery")} >Jewelery</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handleCategoryChange("Electronics")} >Electronics</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handleCategoryChange("Women's clothing")} >Women's Clothing</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+
+                            </label>
+
+                            <label className='mt-2 mr-2 text-sm  text-neutral-500' > Price
+                                <Dropdown >
+                                    <Dropdown.Toggle className='bg-zinc-500 min-w-[11rem] flex justify-between items-center ' variant="secondary" id="dropdown-basic">
+                                        {selectedPrice}
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item onClick={() => handlePriceChange('None')}>None</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handlePriceChange(20)}>less than $20 </Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handlePriceChange(50)}>less than $50</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handlePriceChange(100)}>less than $100</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handlePriceChange(200)}>less than $200</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handlePriceChange(500)}>less than $500</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handlePriceChange(700)}>less than $700</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handlePriceChange(1000)}>less than $1000</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => handlePriceChange(5000)}>less than $5000</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </label>
+
+
                         </div>
 
 
 
-                    )}
 
-                   
 
-                    <div className='flex'>
-                        <label className='mt-2 mr-2 text-sm  text-neutral-500 '> Category
-
-                            <Dropdown >
-                                <Dropdown.Toggle className='bg-zinc-500 min-w-[11rem] flex justify-between items-center' variant="secondary" id="dropdown-basic">
-                                    {selectedCategory}
-                                </Dropdown.Toggle>
-
-                                <Dropdown.Menu>
-                                    <Dropdown.Item onClick={() => handleCategoryChange('None')}>None</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handleCategoryChange("Men's clothing")} >Men's Clothing</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handleCategoryChange("Jewelery")} >Jewelery</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handleCategoryChange("Electronics")} >Electronics</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handleCategoryChange("Women's clothing")} >Women's Clothing</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-
-                        </label>
-
-                        <label className='mt-2 mr-2 text-sm  text-neutral-500' > Price
-                            <Dropdown >
-                                <Dropdown.Toggle className='bg-zinc-500 min-w-[11rem] flex justify-between items-center ' variant="secondary" id="dropdown-basic">
-                                    {selectedPrice}
-                                </Dropdown.Toggle>
-
-                                <Dropdown.Menu>
-                                    <Dropdown.Item onClick={() => handlePriceChange('None')}>None</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handlePriceChange(20)}>less than $20 </Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handlePriceChange(50)}>less than $50</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handlePriceChange(100)}>less than $100</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handlePriceChange(200)}>less than $200</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handlePriceChange(500)}>less than $500</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handlePriceChange(700)}>less than $700</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handlePriceChange(1000)}>less than $1000</Dropdown.Item>
-                                    <Dropdown.Item onClick={() => handlePriceChange(5000)}>less than $5000</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </label>
 
 
                     </div>
-              
+                    <div>
 
 
+                        <Table className='max-w-full' bordered striped>
 
-
-
-
-                </div>
-                <div>
-
-
-                    <Table className='max-w-full' bordered striped>
-
-                        <thead>
-                            <tr>
-                                <th>S.N</th>
-                                <th>Title</th>
-                                <th>Price($)</th>
-                                <th>Description</th>
-                                <th>Category</th>
-                                <th>Ratings</th>
-                            </tr>
-
-
-                        </thead>
-                        <tbody className=''>
-                            {filteredData.map((data) => {
-                                return <tr key={data.id}>
-                                    <td>{data.id}</td>
-                                    <td>{data.title}</td>
-                                    <td>{data.price}</td>
-                                    <td>{data.description}</td>
-                                    <td>{data.category}</td>
-                                    <td>{data.rating.rate >= 2.5 ? <Badge bg="success">{data.rating.rate}</Badge> : <Badge bg="danger">{data.rating.rate}</Badge>}</td>
+                            <thead>
+                                <tr>
+                                    <th>S.N</th>
+                                    <th>Title</th>
+                                    <th>Price($)</th>
+                                    <th>Description</th>
+                                    <th>Category</th>
+                                    <th>Ratings</th>
                                 </tr>
 
-                            })}
 
-                        </tbody>
+                            </thead>
+                            <tbody className=''>
+                                {filteredData.map((data) => {
+                                    return <tr key={data.id}>
+                                        <td>{data.id}</td>
+                                        <td>{data.title}</td>
+                                        <td>{data.price}</td>
+                                        <td>{data.description}</td>
+                                        <td>{data.category}</td>
+                                        <td>{data.rating.rate >= 2.5 ? <Badge bg="success">{data.rating.rate}</Badge> : <Badge bg="danger">{data.rating.rate}</Badge>}</td>
+                                    </tr>
+
+                                })}
+
+                            </tbody>
 
 
-                    </Table>
+                        </Table>
 
 
 
 
+                    </div>
                 </div>
-            </div>
+
+
+
+            }
+
+
+
 
 
 
